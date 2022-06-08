e4fab8a04a5f0e1cad9a0c7001a60283d99de604
jtfreedman <babzibles@gmail.com>
2022-06-07 22:58:58 -0400

    changed AddUser to form

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -3,16 +3,20 @@ import classes from './AddUser.module.css';
 import Button from "./button/Button";
 
 export default function AddUser() {
+    function onSubmitHandler() {
+        
+    }
+
     return (
         <div>
             <Card>
-                <div className={classes.content}>
-                    <h1>Username</h1>
-                    <input />
-                    <h1>Age (Years)</h1>
-                    <input />
-                    <Button>Add User</Button>
-                </div>
+                <form className={classes.content}>
+                    <label>Username</label>
+                    <input id='user'></input>
+                    <label>Age (Years)</label>
+                    <input id='age'></input>
+                    <Button onSubmit={onSubmitHandler}>Add User</Button>
+                </form>
             </Card>
         </div>
     );
