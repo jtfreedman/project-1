ba212010304b95f789c981b30c38ab03566eadd1
jtfreedman <babzibles@gmail.com>
2022-06-07 21:53:39 -0400

    AddUser look replicated, CSS styling, Button added

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,9 +1,19 @@
 import Card from "./card/Card";
+import classes from './AddUser.module.css';
+import Button from "./button/Button";
 
 export default function AddUser() {
     return (
         <div>
-            <Card>hello</Card>
+            <Card>
+                <div className={classes.content}>
+                    <h1>Username</h1>
+                    <input />
+                    <h1>Age (Years)</h1>
+                    <input />
+                    <Button>Add User</Button>
+                </div>
+            </Card>
         </div>
     );
 }
